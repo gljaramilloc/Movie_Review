7d90ed883b7501cbdff94d09132c9b64282027d2
gljaramilloc <gljaramilc@eafit.edu.co>
2026-08-20 13:47:24 -0500

    navbar y footer

--- movie/views.py
+++ movie/views.py
@@ -1,20 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 from .models import Movie
 
-# Create your views here.
 
 def home(request):
-    #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    #return render(request, 'home.html')
-    #return render(request, 'home.html', {'name':'Paola Vallejo'})
     searchTerm = request.GET.get('searchMovie')
+
     if searchTerm:
-        movies = Movie.objects.filter(title__icontains=searchTerm)
+        movies = Movie.objects.filter(
+            title__icontains=searchTerm
+        )
     else:
         movies = Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
+
+    return render(
+        request,
+        'home.html',
+        {
+            'movies': movies,
+            'searchTerm': searchTerm
+        }
+    )
+
 
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>')
+    return render(request, 'about.html')
+
+
+def signup(request):
+    email = request.GET.get('email')
+
+    return render(
+        request,
+        'signup.html',
+        {
+            'email': email
+        }
+    )
